5272d28e84b93ca018b1c70c2c84646c1b9ad3c5
DonxYu <dxy.message@gmail.com>
2019-03-20 00:58:08 +0800

    test

---
+++ chapter-1-spring-boot-quickstart/src/main/java/demo/springboot/web/HelloWorldController.java
@@ -0,0 +1,15 @@
+package demo.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value="/helloworld",method = RequestMethod.GET)
+    @ResponseBody
+    public String hahaha() {
+        return "Hello World";
+    }
+}
